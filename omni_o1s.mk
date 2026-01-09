2a316dc9ff0acdd618b494220e51c6cce3ee3a7f
GoRhanHee <gorhanhee01@gmail.com>
2026-01-09 23:44:25 +0900

    o1s.mk: edit config

--- omni_o1s.mk
+++ omni_o1s.mk
@@ -10,15 +10,15 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from o1s device
 $(call inherit-product, device/samsung/o1s/device.mk)
 
 PRODUCT_DEVICE := o1s
-PRODUCT_NAME := omni_o1s
+PRODUCT_NAME := twrp_o1s
 PRODUCT_BRAND := samsung
-PRODUCT_MODEL := SM-G991N
+PRODUCT_MODEL := "Galaxy S21 5G"
 PRODUCT_MANUFACTURER := samsung
 
 PRODUCT_GMS_CLIENTID_BASE := android-samsung-ss
