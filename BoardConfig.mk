b23939c5e3616048bf2390d883de4135801ca23b
GoRhanHee <gorhanhee01@gmail.com>
2026-01-10 01:18:05 +0900

    config: setting security patch level

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -80,15 +80,12 @@ TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2021-08-01
-
-# Verified Boot
-BOARD_AVB_ENABLE := false
-
-# Hack: prevent anti rollback
 PLATFORM_SECURITY_PATCH := 2099-12-31
 VENDOR_SECURITY_PATCH := 2099-12-31
 PLATFORM_VERSION := 16.1.0
+
+# Verified Boot
+BOARD_AVB_ENABLE := false
 
 # TWRP Configuration
 TW_DEVICE_VERSION := GoRhanHee
